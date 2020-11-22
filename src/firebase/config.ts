39be9fc93c49ff701c3f58f9875dc4fd2a034b88
Oscar Jin <kintininn@gmail.com>
2020-11-22 20:03:03 +0900

    refactor

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,16 +1,8 @@
 import * as firebase from "firebase/app";
+import config from "./config.json";
 
 //  ─────────────────────────────────────────────────────────── PROFILE ───┐
-const configuration = {
-  apiKey: "AIzaSyBlVpu45rAGyIgDUNhWR7P9JjyrSiGwJN0",
-  authDomain: "lacoms-production-database.firebaseapp.com",
-  databaseURL: "https://lacoms-production-database.firebaseio.com",
-  projectId: "lacoms-production-database",
-  storageBucket: "lacoms-production-database.appspot.com",
-  messagingSenderId: "218924369933",
-  appId: "1:218924369933:web:7659e0aeca506a02dced44",
-  measurementId: "G-64TY1NJYWG",
-};
+const configuration = config;
 // <───────────────────────────────────────────────────────────────────────┘
 
 //  ────────────────────────────────────────────────────────────── INIT ───┐
